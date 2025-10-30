4f87856cb4a6515e4ed7f4b3b4ef9861cc82b915
Sudeepta Bal <sudeepta95bal@gmail.com>
2025-10-30 09:51:25 +0000

    changes committed for the Event.fs model

--- server/HealthcareFSharpApi/Models/Event.fs
+++ server/HealthcareFSharpApi/Models/Event.fs
@@ -1,1 +1,71 @@
-// event / group therapy session models 
+namespace MyFSharpApi.Models
+
+open MongoDB.Bson
+open MongoDB.Bson.Serialization
+open MongoDB.Bson.Serialization.Attributes
+
+[<BsonIgnoreExtraElements>]
+type NewEvent = {
+    category: string
+    title: string
+    host:string
+    startDate:System.DateTime
+    endDate:System.DateTime
+    location:string
+    // [<BsonId>]
+    // [<BsonRepresentation(BsonType.ObjectId)>]
+    // Id: ObjectId
+    detail: string
+    filename: string
+    [<BsonElement("__v")>]
+    Version: int
+}
+
+type Event = {
+    category: string
+    title: string
+    host:ObjectId
+    startDate:System.DateTime
+    endDate:System.DateTime
+    location:string
+    [<BsonId>]
+    [<BsonRepresentation(BsonType.ObjectId)>]
+    _id: ObjectId
+    detail: string
+    filename: string
+    [<BsonElement("__v")>]
+    Version: int
+}
+
+type GetEvent = {
+    category: string
+    title: string
+    [<BsonRepresentation(BsonType.ObjectId)>]
+    host:string
+    startDate:System.DateTime
+    endDate:System.DateTime
+    location:string
+    [<BsonId>]
+    [<BsonRepresentation(BsonType.ObjectId)>]
+    _id: ObjectId
+    detail: string
+    filename: string
+    [<BsonElement("__v")>]
+    Version: int
+}
+
+[<BsonIgnoreExtraElements>]
+type UpdateEvent = {
+    category: string
+    title: string
+    host:string
+    startDate:System.DateTime
+    endDate:System.DateTime
+    location:string
+    _id:string
+    detail: string
+    filename: string
+    // [<BsonElement("__v")>]
+    // Version: int
+}
+ 
